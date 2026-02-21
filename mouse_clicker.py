3a1e831bbeac6a9e410e37366ce8e18a42cf2180
saintwhales <60368143+saintwhales@users.noreply.github.com>
2026-02-21 10:13:08 +0000

    Add files via upload

--- mouse_clicker.py
+++ mouse_clicker.py
@@ -9,17 +9,14 @@ alien = Actor('alien')
 
 def draw():
     screen.fill(color = (128,0,0))
-
+    
+    alien.draw()
     screen.draw.text(f"Score: {count}", center = (400,10), fontsize = 30)
-
-def place_alien():
-    alien.x = (mouseX)
-    alien.y = (mouseY)
 
 def on_mouse_down(pos):
     global count
-
-    place_alien()
+    
+    alien.pos = pos
     count += 1
 
 pgzrun.go()
