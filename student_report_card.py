b957f4255c9d77de0e26d58b07f324dd9d0c5209
saintwhales <60368143+saintwhales@users.noreply.github.com>
2026-03-14 10:03:58 +0000

    Add files via upload

---
+++ student_report_card.py
@@ -0,0 +1,12 @@
+student = (95, 74, 82, 98, 87)
+
+for x in student:
+    print(x, end = ' ')
+
+maths, english, science, computing, geography = student
+
+print('\nmaths:', maths)
+print('english:', english)
+print('science:', science)
+print('computing:', computing)
+print('geography:', geography)
