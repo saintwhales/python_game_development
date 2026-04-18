c08544e277828d96ef6eed10de9cf066d4089f77
saintwhales <60368143+saintwhales@users.noreply.github.com>
2026-04-18 10:03:24 +0100

    Add files via upload

---
+++ sets_practice.py
@@ -0,0 +1,12 @@
+#sets A and B
+A = {2,3,3,4,5,4,3,5,7}
+B = {4,3,6,7,8,8,9,9,10}
+
+#set operations
+print(A.union(B))
+
+print(A.intersection(B))
+
+print(A.difference(B))
+
+print(A.symmetric_difference(B))
