1f2c40f7fc0d3430ca77f8b220db5ee5da496a3e
saintwhales <60368143+saintwhales@users.noreply.github.com>
2026-05-02 10:57:50 +0100

    Add files via upload

--- recycle_marathon.py
+++ recycle_marathon.py
@@ -1,7 +1,7 @@
 import pgzrun
 import random
 
-FONT_option = (225,225,225)
+FONT_option = (225, 225, 225)
 WIDTH = 1000
 HEIGHT = 800
 
@@ -21,10 +21,11 @@ current_level = 1
 items = []
 animations = []
 
+
 def draw():
     global items, current_level, game_over, game_complete
     screen.clear()
-    screen.blit("bground", (0,0))
+    screen.blit("bground", (0, 0))
     if game_over:
         display_message("GAME OVER", "Try Again")
     elif game_complete:
@@ -32,11 +33,13 @@ def draw():
     else:
         for item in items:
             item.draw()
-        
+
+
 def update():
     global items
     if len(items) == 0:
         items = make_items(current_level)
+
 
 def make_items(number_of_extra_items):
     items_to_create = get_option_to_create(number_of_extra_items)
@@ -47,6 +50,7 @@ def make_items(number_of_extra_items):
 
     return new_items
 
+
 def get_option_to_create(number_of_extra_items):
     items_to_create = ["paper"]
 
@@ -55,12 +59,14 @@ def get_option_to_create(number_of_extra_items):
         items_to_create.append(random_option)
     return items_to_create
 
+
 def create_items(items_to_create):
     new_items = []
     for option in items_to_create:
         item = Actor(option + "img")
         new_items.append(item)
     return new_items
+
 
 def layout_items(items_to_layout):
     number_of_gaps = len(items_to_layout)
@@ -70,17 +76,22 @@ def layout_items(items_to_layout):
         new_x_pos = (index + 1) * gap_size
         item.x = new_x_pos
 
+
 def animate_items(items_to_animate):
     global animations
     for item in items_to_animate:
         duration = START_SPEED - current_level
         item.anchor = ("center", "bottom")
-        animation = animate(item, duration = duration, on_finished = handle_game_over, y = HEIGHT)
+        animation = animate(
+            item, duration=duration, on_finished=handle_game_over, y=HEIGHT
+        )
         animations.append(animation)
+
 
 def handle_game_over():
     global game_over
     game_over = True
+
 
 def on_mouse_down(pos):
     global items, current_level
@@ -90,3 +101,30 @@ def on_mouse_down(pos):
                 handle_game_over()
             else:
                 handle_game_over()
+
+
+def handle_game_complete():
+    global current_level, items, animations, game_complete
+    stop_animations(animations)
+    if current_levels == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level += 1
+        items = []
+        animations = []
+
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animations.running:
+            animation.stop()
+
+
+def display_message(heading_text, sub_heading_text):
+    screen.draw.text(heading_text, font_size=60, center=CENTER, color="white")
+    screen.draw.text(
+        sub_heading_text, fontsize=60, center=(CENTER_X, CENTER_Y + 30), color="white"
+    )
+
+
+pgzrun.go()
